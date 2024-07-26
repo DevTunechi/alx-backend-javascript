6cc6229cf9e241a393b0d82405e8805646983e5d
Olatunji Oluwadare <tunjidare2@yahoo.com>
2024-07-26 22:40:10 +0100

    EV Car

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -2,24 +2,12 @@
 import Car from './10-car';
 
 export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this._range = range;
-  }
+	constructor(brand, motor, color, range) {
+		super(brand, motor, color);
+		this._range = range;
+	}
 
-  get range() {
-    return this._range;
-  }
-
-  set range(argVal) {
-    if (typeof argVal === 'string') this._range = argVal;
-    else throw new TypeError('Range must be a string');
-  }
-
-  cloneCar() {
-    const Sps = super.constructor[Symbol.species];
-    const carClone = new Sps();
-
-    return carClone;
-  }
+	cloneCar() {
+		return new Car();
+	}
 }
