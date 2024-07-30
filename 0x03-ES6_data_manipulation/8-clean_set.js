4c0e5691cc49e584f4b39b952f747cc501f1bac8
Olatunji <tunjidare2@yahoo.com>
2024-07-30 08:52:41 +0100

    Clean Set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+function cleanSet(set, startString) {
+	  if (!startString) {
+		      return '';
+		    }
+
+	  const result = [];
+
+	  for (let value of set) {
+		      if (value.startsWith(startString)) {
+			            result.push(value.slice(startString.length));
+			          }
+		    }
+
+	  return result.join('-');
+}
+
+export default cleanSet;
