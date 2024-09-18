a4171931e95e1b79e524e0806863aab2224c8ef7
Olatunji Oluwadare <tunjidare2@yahoo.com>
2024-09-18 14:09:42 +0100

    Database added and Files Testing

---
+++ 0x05-Node_JS_basic/2-main_0.js
@@ -0,0 +1,11 @@
+2-read_file.js:9
+    throw new Error('Cannot load the database');
+    ^
+
+Error: Cannot load the database
+...
+bob@dylan:~$
+bob@dylan:~$ cat 2-main_1.js
+const countStudents = require('./2-read_file');
+
+countStudents("database.csv");
